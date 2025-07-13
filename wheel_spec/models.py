3bcf0aab105ed1c10a7c27ccb4ffe49a582e0005
khatrikajal <kajalkhatri9373@gmail.com>
2025-07-13 14:00:21 +0530

    updated models.py and readme file

--- wheel_spec/models.py
+++ wheel_spec/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class WheelSpecification(models.Model):
-    formNumber = models.CharField(max_length=50)
+    formNumber = models.CharField(max_length=50, unique=True)
     submittedBy = models.CharField(max_length=50)
     submittedDate = models.DateField()
     fields = models.JSONField()  # store the nested fields dict
